6d9c1cde01e7ed5b97c673e5341a665594ba5c85
Darren Henderson <darrenh@ccsj.edu>
2021-07-26 02:22:45 -0500

    Rearranged order of charts on index view. Commented out selected chart types.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,36 +34,35 @@
         'categories' => ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
         'data' => $data,
     ];
-    
+
     @endphp
 
         <h2>Line Alt</h2>
         <line-chart-alt :series='@json($series)'></line-chart-alt>
         <!-- <line-chart-alt :series="[10, 20, 30, 40, 50, 40, 30, 20, 10, 15, 15, 30]"></line-chart-alt> -->
 
-        <h2>Line</h2>
-        <line-chart title="Line Chart - Constant" :values="[5, 5, 5]"></line-chart>
+        <h2>Column</h2>
+        <column-chart></column-chart>
+        
+        <h2>Stacked Column</h2>
+        <stacked-column-chart></stacked-column-chart>
 
         <h2>Stacked Percentage Column</h2>
         <stacked-percentage-column-chart></stacked-percentage-column-chart>
         
-        <h2>Stacked Column</h2>
-        <stacked-column-chart></stacked-column-chart>
-        
-        <h2>Pie</h2>
-        <pie-chart></pie-chart>
-        
-        <h2>Column</h2>
-        <column-chart></column-chart>
-        
-        <h2>Combo</h2>
-        <combo-chart></combo-chart>
+        <!-- <h2>Line</h2> -->
+        <!-- <line-chart title="Line Chart - Constant" :values="[5, 5, 5]"></line-chart> -->
 
-        <h2>More Lines</h2>
-        <line-chart title="Line Chart 1" :values="[3, 2, 1]"></line-chart>
+        <!-- <h2>Pie</h2> -->
+        <!-- <pie-chart></pie-chart> -->
         
-        <line-chart :values="[10, 15, 5]"></line-chart>
+        <!-- <h2>Combo</h2> -->
+        <!-- <combo-chart></combo-chart> -->
+
+        <!-- <h2>More Lines</h2> -->
+        <!-- <line-chart title="Line Chart 1" :values="[3, 2, 1]"></line-chart> -->
         
+        <!-- <line-chart :values="[10, 15, 5]"></line-chart> -->
         
         <!-- <chart-demo title="My Title"
                     :values="[10, 0, 8, 2, 6, 4, 5, 5]">
